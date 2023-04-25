797955e5100be4d8d03d906f9af3bedfedf9cdc7
minaraafat21 <106335253+minaraafat21@users.noreply.github.com>
2023-04-26 01:39:35 +0200

    Delete untitled directory

--- untitled/src/Q0.java
+++
@@ -1,6 +0,0 @@
-public class Q0 {
-    public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("Mina Raafat");
-    }
-}
--- untitled/src/Q6.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Q6 {
-    public static void main(String[] args) {
-        System.out.println("please enter degree in Fahrenheit");
-        Scanner sc=new Scanner(System.in);
-        double fTemp= sc.nextInt();
-        double cTemp=(5.0/9.0)*(fTemp-32);
-        System.out.println("converted value to celesius = "+cTemp);
-    }}
